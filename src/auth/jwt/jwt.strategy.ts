0eb043e1f223f3da4a56d17f9117f2ff1a6d111e
xPotter777 <senkivsasa@gmail.com>
2023-10-30 14:41:43 +0200

    Dockerize

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
+      secretOrKey: config.get('jwt.secret'),
     });
   }
 
